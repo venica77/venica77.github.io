a014c4d5f103ead2c26c30badaae3b327da694e6
venica77 <123378680+venica77@users.noreply.github.com>
2023-03-15 19:27:56 -0300

    Add files via upload

---
+++ curriculum.js
@@ -0,0 +1,39 @@
+document.getElementById('boton').addEventListener('click', function(){
+    document.getElementById("demo").innerHTML=
+    "Soy un experimentado representante de Servicio al Cliente con un historial \
+    comprobable de resolución de problemas complejos rápidamente y ganando la lealtad del cliente"
+    ;
+});
+
+document.getElementById('boton_1').addEventListener('click', function(){
+    document.getElementById("demo").innerHTML=
+    "Resolución de conflictos - Excelentes habilidades de comunicación- \
+    Detección de problemas - Servicio y soporte- Sólida ética laboral-\
+     Gran atención al detalle"
+    ;
+});
+document.getElementById('boton_2').addEventListener('click', function(){
+    document.getElementById("demo").innerHTML="Bruno Morales, Telecomunicaciones A y M\
+    (11) 7455 1234 hola@sitioincreible.com.ar || Regina Perales, Soluciones Tecno(11) 7456 2544\
+    hola@sitioincreible.com.ar";
+});
+document.getElementById('boton_3').addEventListener('click', function(){
+    document.getElementById("demo").innerHTML="Líder de Servicio al Cliente-\
+    Resolución de un promedio de 300 consultas semanales-Ayuda a la empresa\
+     a recibir calificaciones altas en servicio al cliente- Capacitación a \
+     nuevos representantes de Servicio al Cliente-TELECOMUNICACIONES LEMOS \
+     2020 AL PRESENTE|| Agente de Servicio al Cliente- Atención de consultas\
+      de Servicio al Cliente en tiempo y forma- Logro de una calificación de \
+      satisfacción del cliente del 98 %- Recomendaciones para mejorar procesos\
+       internos SOLUCIONES TECNOLÓGICAS BECNEL 2017 A 2020"
+ ;
+    });
+document.getElementById('boton_4').addEventListener('click', function(){
+        document.getElementById("demo").innerHTML="Universidad de Moreno\
+        LICENCIADO EN ADMINISTRACIÓN EMPRESARIAL, 2016- Promedio de 9- Mención honorífica consistente\
+        - Editora del periódico universitario- Miembro del equipo universitario de nado durante 3 años\
+        Escuela Media de La Tablada- Promedio de 9- Capitan del equipo de natación- Escritor del periódico\
+         de la preparatoria- Diseñador de maquetas del anuario BACHILLER, PROMOCIÓN 2012";
+        });
+    
+   
